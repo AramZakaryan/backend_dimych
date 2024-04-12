0a1c10fbe090872ef94dd1257a302347ced73057
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-04-13 00:41:58 +0400

    Result du nouveau 6

--- server.js
+++ server.js
@@ -1,28 +1,49 @@
 const http = require("http")
+const fs = require("fs")
 
+const delay = (ms) => {
+    return new Promise(res => {
+        setTimeout(() => res(), ms)
+    })
+}
+
+const myReadFile = (path) => {
+    return new Promise((res, rej) => {
+        fs.readFile(path, (err, data) => {
+            if (err) {
+                rej(err)
+            } else {
+                res(data)
+            }
+        })
+    })
+}
 
 const server = http.createServer(
-    (request, response) => {
-
+    async (request, response) => {
         switch (request.url) {
-            case "/":{
-                response.write("Hello, welcome !")
+            case "/home": {
+                try {
+                    const data = await myReadFile("pages/home.html")
+                    response.write(data)
+                } catch (err) {
+                    response.write("some error")
+                } finally {
+                    response.end()
+                }
                 break
             }
-            case "/students": {
-                response.write("STUDENTS")
-                break
-            }
-            case "/courses":{
-                response.write("COURSES")
+            case "/about": {
+                await delay(3000)
+                response.write("this is about (just a statement)")
+                response.end()
                 break
             }
             default: {
                 response.write("404 not found")
+                response.end()
             }
         }
-
-        response.end()
     }
 )
 
